1279cc5d2ba710c9a793530fc78d2438157930b5
tran_b <tran.benjamin.bt@gmail.com>
2019-01-26 08:59:05 +0100

    add: Game scene add: Lobby

--- GGJ2019Isart/Assets/Scripts/Singleton/GameManagerSingleton.cs
+++ GGJ2019Isart/Assets/Scripts/Singleton/GameManagerSingleton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManagerSingleton : Singleton<GameManagerSingleton>
 {
@@ -9,4 +10,31 @@ public class GameManagerSingleton : Singleton<GameManagerSingleton>
 
     // Then add whatever code to the class you need as you normally would.
     public APathNode[] pathNode;
+
+	public Dictionary<int, int> indexSlotDictionnary;
+	public GameObject playerPrefab;
+
+	private void Start()
+	{
+		SceneManager.sceneLoaded += this.OnSceneLoaded;
+	}
+
+	public void SpawnPlayer()
+	{
+		int nbPlayer = this.indexSlotDictionnary.Count + 1;
+
+		Debug.Log("NB PLAYERS: " + (nbPlayer -1));
+		for (int i = 1; i < nbPlayer; i++)
+		{
+			MainController controller = Instantiate(playerPrefab).GetComponent<MainController>();
+
+			Debug.Log("SPAWN PLAYER: " + i + " FROM SLOT: " + this.indexSlotDictionnary[i]);
+			controller.Init(i, this.indexSlotDictionnary[i]);
+		}
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		this.SpawnPlayer();
+	}
 }
