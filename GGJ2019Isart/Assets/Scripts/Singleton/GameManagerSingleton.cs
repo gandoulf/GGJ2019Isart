da7d8dc5a45ba532eeb9da4bb9083ef1cc0166a1
tran_b <tran.benjamin.bt@gmail.com>
2019-01-27 06:09:05 +0100

    wip: Gain score with trash

--- GGJ2019Isart/Assets/Scripts/Singleton/GameManagerSingleton.cs
+++ GGJ2019Isart/Assets/Scripts/Singleton/GameManagerSingleton.cs
@@ -124,6 +124,7 @@ public class GameManagerSingleton : MonoBehaviour
 		this.score += gain;
 		this.hud.UpdateScore(this.score);
 		this.IncRage(gain);
+		this.hud.UpdateRage(this.rage);
 	}
 
 	private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
